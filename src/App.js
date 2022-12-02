7a0aaa8183adcefb4631b159ecf57d5f82f0a14a
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-12-02 15:34:36 -0500

    My first React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// All your imports should be at the top
+// import logo from './logo.svg';
+import Header from "./components/Header";
+import Player from "./components/Player";
+import "./App.css";
+import Board from "./components/Board";
+// import Square from "./components/Square";
 
-function App() {
+export default function App() {
+  // What is JSX? JSX is a hybrid of JavaScript and HTML.  In JSX, we can only return one parent element. If we want to return more than one element, we have to wrap it in a parent element. Typically a div or you can use a React.Fragment
+
+  // PROPS: we add props by adding key value pairs inside our rendered components. They look like attributes. We can call props anything that we want.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {/* We only adding one header because we only need 1 title */}
+      <div className="container">
+        <Player whichPlayer="X" />
+        <Player whichPlayer="O" />
+      </div>
+
+      {/* The reason we are adding 2 players is because we have player X & O */}
+      <Board />
+      
+    </>
   );
 }
 
-export default App;
+// if we writing it this way all we have to do is add export before the const.
+//NO NEED TO ADD DEFAULT
+// const App = () => {
+//   return (
+//     <h1>
+//       React Tac Toe
+//     </h1>
+//   )
+// }
+
+// Forms of writing the app function in Vanilla JS
+// function declarations using the function
+// function expressions
+// arrow functions
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+    <h1>
+        React Tic Tac Toe Header
+    </h1>
+    )
+}
+
+export default Header
